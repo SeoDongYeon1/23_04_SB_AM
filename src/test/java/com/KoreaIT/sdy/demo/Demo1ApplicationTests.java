e09172f6e6a591f2e8824398830a121a27fd2988
SeoDongYeon1 <ehddus2655@gmail.com>
2023-04-13 14:36:24 +0900

    실행환경 세팅

---
+++ src/test/java/com/KoreaIT/sdy/demo/Demo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KoreaIT.sdy.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
