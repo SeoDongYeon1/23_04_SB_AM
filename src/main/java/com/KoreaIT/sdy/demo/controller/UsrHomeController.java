56392cc26b5433ba44d871fd4085ad20b7ba5747
SeoDongYeon1 <ehddus2655@gmail.com>
2023-04-13 15:36:36 +0900

    count값 초기화 액션

--- src/main/java/com/KoreaIT/sdy/demo/controller/UsrHomeController.java
+++ src/main/java/com/KoreaIT/sdy/demo/controller/UsrHomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UsrHomeController {
-	int count = 0;
+	private int count;
 	
 	public UsrHomeController() {
 		count = 0;
@@ -35,5 +35,11 @@ public class UsrHomeController {
 	@ResponseBody
 	public int showMain4() {
 		return count++;
+	}
+	@RequestMapping("/usr/home/main5")
+	@ResponseBody
+	public String showMain5() {
+		count = 0;
+		return "count의 값이 0으로 초기화 됩니다.";
 	}
 }
